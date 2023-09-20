a531b286fadf227e95cffa99c59ef424336b9fcf
Fallen_C0de <spinthegamer760@gmail.com>
2023-09-20 15:16:29 -0400

    Getting car make

--- Car.java
+++ Car.java
@@ -3,8 +3,14 @@ public class Car{
     public static void main(String[]args){
         CarDirectroy Car = new CarDirectroy();
 
+        //Getting Random Model
         Car.SetRandModel();
-        String Make = Car.GetRandModel();
+        String Model = Car.GetRandModel();
+        System.out.println(Model);
+
+        //Getting Random Make
+        Car.SetRandMake(Model);
+        String Make = Car.getRandMake();
         System.out.println(Make);
     }
     
--- CarDirectroy.java
+++ CarDirectroy.java
@@ -2,17 +2,20 @@ import java.util.Random;
 
 public class CarDirectroy {
 
-    //Var for Make and Model of Generated Car
+    //Var for Make and Model of Generated Car (Macan Has Only One Model)
     String CarMake = "";
     String CarModel = "";
 
+    //Models
     String[] Models = {"718", "911", "Taycan", "Panamera", "Macan", "Cayenne"};
+
+    //Makes
     String[] Makes718 = {"Cayman", "Boxter", "GT4 RS", "Spyder RS"};
     String[] Makes911 = {"Carrera", "Carrera Cabriolet", "Targa", "Turbo", "Turbo Cabriolet", "Dakar", "GT3", "GT3 RS", "S/T"};
     String[] MakesTaycan = {"Taycan", "Cross Turismo", "Sport Turismo"};
     String[] MakesPanamera = {"Panamera", "Sport Turismo", "Executive"};
-    String[] MakesMacan = {"Macan"};
     String[] MakesCayenne = {"Cayenne", "Cayenne Coupe"};
+
 
     //Constuctor
     public CarDirectroy(){
@@ -30,6 +33,50 @@ public class CarDirectroy {
     //Getting the Random Model
     public String GetRandModel(){
         return CarMake;
+    }
+
+    //Setting the Random Make
+    public void SetRandMake(String CurrentModel){
+
+        //Setting Make If Model is 718
+        if(CurrentModel.equals("718")){
+            Random Seven18 = new Random();
+            CarModel = Makes718[Seven18.nextInt(Makes718.length)];
+        }
+
+        //Setting Make If Model is 911
+        else if(CurrentModel.equals("911")){
+            Random Nine11 = new Random();
+            CarModel = Makes911[Nine11.nextInt(Makes911.length)];
+        }
+
+        //Setting Make If Model is Taycan
+        else if(CurrentModel.equals("Taycan")){
+            Random Taycan = new Random();
+            CarModel = MakesTaycan[Taycan.nextInt(MakesTaycan.length)];
+        }
+    
+        //Setting Make If Model is Panamera
+        else if(CurrentModel.equals("Panamera")){
+            Random Panamera = new Random();
+            CarModel = MakesPanamera[Panamera.nextInt(MakesPanamera.length)];
+        }
+        
+        //Setting Make If Model is Cayenne
+        else if(CurrentModel.equals("Cayenne")){
+            Random Cayenne = new Random();
+            CarModel = MakesCayenne[Cayenne.nextInt(MakesCayenne.length)];
+        }
+
+        else{
+            CarModel = "";
+        }
+        
+    }
+
+    //Getting The Random Make
+    public String getRandMake(){
+        return CarModel;
     }
 }
 
