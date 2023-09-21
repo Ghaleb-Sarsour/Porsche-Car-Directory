a62d6b42cfb7f0e5f5cb70682cc1dabb01bbd37d
Fallen_C0de <spinthegamer760@gmail.com>
2023-09-21 11:28:20 -0400

    added user input and ability to generate more than one car at a time

--- Car.java
+++ Car.java
@@ -1,21 +1,35 @@
+import java.util.Scanner;
+
 public class Car{
 
     public static void main(String[]args){
         CarDirectroy Car = new CarDirectroy();
 
-        //Getting Random Model
-        Car.setRandModel();
-        String Model = Car.getRandModel();
+        //Welcome Message
+        System.out.println("Welcome to the Porsche Directory!");
 
-        //Getting Random Make
-        Car.setRandMake(Model);
-        String Make = Car.getRandMake();
+        //Gathering User Input on Generation Choice
+        Scanner CarGen = new Scanner(System.in);
+        System.out.print("How Many Cars Do You Want To Generate?: ");
+        int NumofCars = CarGen.nextInt();
 
-        //Getting Random Trim
-        Car.setRandTrim(Model, Make);
-        String Trim = Car.getRandTrim();
-        //Printing Car
-        System.out.print("Porsche " + Model + " " + Make + " " + Trim);
+        //For loop based off of number of cars chosen by user
+        for(int i = 0; i < NumofCars; i++){
+
+
+            //Getting Random Model
+            Car.setRandModel();
+            String Model = Car.getRandModel();
+
+            //Getting Random Make
+            Car.setRandMake(Model);
+            String Make = Car.getRandMake();
+
+            //Getting Random Trim
+            Car.setRandTrim(Model, Make);
+            String Trim = Car.getRandTrim();
+            //Printing Car
+            System.out.println("Porsche " + Model + " " + Make + " " + Trim);
+        }
     }
-    
 }
